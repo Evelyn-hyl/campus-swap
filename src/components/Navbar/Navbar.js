f0d9f7a8a430d41e39f35933152a5f143f348b21
Evelyn-hyl <evvvta@gmail.com>
2025-02-19 22:44:04 -0800

    Added School Search Dropdown / 50 States Data

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,29 @@ import { FaSearch } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
 import { IconContext } from "react-icons"
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import states from '../../data/usStates'
 import './Navbar.css';
 
 export function Navbar({ handleMenuClick, handleCartClick }) {
   const [isSchoolDropdownOpened, setIsSchoolDropdownOpened] = useState(false);
+  const schoolInputRef = useRef(null);
+
+
+  const handleSchoolSelect = (stateName) => {
+    if (schoolInputRef.current) {
+      schoolInputRef.current.value = stateName;
+    }
+    setIsSchoolDropdownOpened(false);
+  }
+
+  const handleSchoolDropdown = () => {
+    setIsSchoolDropdownOpened((prev) => !prev);
+  }
+
     return (
         <nav className='nav-bar'>
+
           <div className='logo-wrapper'>
             <button id='menu-button' onClick={handleMenuClick}>
               <IconContext.Provider value={{ className:'menu-icon'}}>
@@ -19,26 +35,40 @@ export function Navbar({ handleMenuClick, handleCartClick }) {
               </IconContext.Provider>
             </button>
           </div>
-          <div className='search-bar-wrapper'>
-            <div className='logo-wrapper--location'>
-              <IconContext.Provider value={{ className: 'location-icon'}}>
-                <div>
-                  <FaLocationDot />
-                </div>
-              </IconContext.Provider>
-            </div>
-            <input className='search-box school' type='text' placeholder='Enter School...' onFocus={() => setIsSchoolDropdownOpened(true)}/>
 
-            <div className='logo-wrapper--search'>
-              <IconContext.Provider value={{ className: "location-icon"}}>
-                <div>
-                  <FaSearch />
-                </div>
-              </IconContext.Provider>
+          <div className='search-bar-wrapper'>
+            <div className='school-search-bar-wrapper'>
+              <div className='logo-wrapper--location'>
+                <IconContext.Provider value={{ className: 'location-icon'}}>
+                  <div>
+                    <FaLocationDot />
+                  </div>
+                </IconContext.Provider>
+              </div>
+              <input className='search-box school' type='text' ref={schoolInputRef} placeholder='Enter School...' onFocus={handleSchoolDropdown}/>
+              <div className={`search-box-dropdown ${isSchoolDropdownOpened ? 'school-dropdown--open' : ''}`}>
+                <p className='dropdown-inline-title'>Select State</p>
+                <div className='divider'></div>
+                <ul className="dropdown-options">
+                  {states.slice(0, 10).map((item, index) => (
+                    <li key={index} onClick={() => handleSchoolSelect(item.name)}>{ item.name }</li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <input className='search-box search' type='text' placeholder='Search...'></input>
+
+            <div className='search-search-bar-wrapper'>
+              <div className='logo-wrapper--search'>
+                <IconContext.Provider value={{ className: "location-icon"}}>
+                  <div>
+                    <FaSearch />
+                  </div>
+                </IconContext.Provider>
+              </div>
+              <input className='search-box search' type='text' placeholder='Search...'></input>
+            </div>
           </div>
-          <div className={`search-box-dropdown ${isSchoolDropdownOpened ? 'school-dropdown--open' : ''}`}></div>
+
           <div className='logo-wrapper'>
             <button className='shopping-bag-button' onClick={handleCartClick}>
             <IconContext.Provider value={{ className: "shopping-bag-icon" }}>
