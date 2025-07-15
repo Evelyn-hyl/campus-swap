c51fa1fca3756fb6124ec1c664b1f79c1bf81522
Evelyn-hyl <evvvta@gmail.com>
2025-07-14 17:48:43 -0700

    Initial commit with Spring Boot setup

---
+++ server/campus-swap-api/src/main/java/com/campusswap/campus_swap_api/CampusSwapApiApplication.java
@@ -0,0 +1,13 @@
+package com.campusswap.campus_swap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSwapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusSwapApiApplication.class, args);
+	}
+
+}
---
+++ server/campus-swap-api/src/test/java/com/campusswap/campus_swap_api/CampusSwapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusswap.campus_swap_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSwapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
